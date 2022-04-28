56f1cfc9e56f747ee5acf5eaa4cab44ac67d821a
FelipePicolo <luizfelipe.picolo@uvic.cat>
2022-04-28 16:53:03 +0200

    HC595_SerialControl Working

--- Software documents/Code/HC595_SerialControl/SerialRelay.h
+++ Software documents/Code/HC595_SerialControl/SerialRelay.h
@@ -4,8 +4,10 @@
 /*
 	   RoboCore Serial Relay Library
 		    (v1.0 - 20/05/2015)
+
   Serial Relay functions for Arduino
     (tested with Arduino 1.0.1)
+
   Copyright 2015 RoboCore (François) ( http://www.RoboCore.net )
   
   ------------------------------------------------------------------------------
@@ -13,10 +15,12 @@
   it under the terms of the GNU Lesser General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU Lesser General Public License for more details.
+
   You should have received a copy of the GNU Lesser General Public License
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
   ------------------------------------------------------------------------------
@@ -202,3 +206,7 @@ class SerialRelay {
 
 
 #endif // RC_SERIAL_RELAY_H
+
+
+
+
--- Software documents/Code/Test74HC595/Test74HC595.ino
+++ Software documents/Code/Test74HC595/Test74HC595.ino
@@ -1,8 +1,10 @@
 int latchPin = 5;  // Latch pin of 74HC595 is connected to Digital pin 5
 int clockPin = 6; // Clock pin of 74HC595 is connected to Digital pin 6
-int dataPin = 4;  // Data pin of 74HC595 is connected to Digital pin 4
+int dataPinG = 4;  // Data pin of 74HC595 is connected to Digital pin 4
+int dataPinB = 3;  // Data pin of 74HC595 is connected to Digital pin 3
  
-byte leds = 0;    // Variable to hold the pattern of which LEDs are currently turned on or off
+byte ledsB = 0;    // Variable to hold the pattern of which LEDs are currently turned on or off
+byte ledsG = 0;    // Variable to hold the pattern of which LEDs are currently turned on or off
 
 /*
  * setup() - this function runs once when you turn your Arduino on
@@ -12,7 +14,8 @@ void setup()
 {
   // Set all the pins of 74HC595 as OUTPUT
   pinMode(latchPin, OUTPUT);
-  pinMode(dataPin, OUTPUT);  
+  pinMode(dataPinG, OUTPUT);  
+  pinMode(dataPinB, OUTPUT); 
   pinMode(clockPin, OUTPUT);
 }
 
@@ -21,14 +24,18 @@ void setup()
  */
 void loop() 
 {
-  leds = 0; // Initially turns all the LEDs off, by giving the variable 'leds' the value 0
+  ledsB = 0; // Initially turns all the LEDs off, by giving the variable 'leds' the value 0
+  ledsG = 0; // Initially turns all the LEDs off, by giving the variable 'leds' the value 0
   updateShiftRegister();
   delay(250);
+  int j = 7;
   for (int i = 0; i < 8; i++) // Turn all the LEDs ON one by one.
   {
-    bitSet(leds, i);    // Set the bit that controls that LED in the variable 'leds'
+    bitSet(ledsG, j);
+    bitSet(ledsB, i);    // Set the bit that controls that LED in the variable 'leds'
     updateShiftRegister();
-    delay(250);
+    j--;
+    delay(10000);
   }
 }
 
@@ -38,6 +45,6 @@ void loop()
 void updateShiftRegister()
 {
    digitalWrite(latchPin, LOW);
-   shiftOut(dataPin, clockPin, MSBFIRST, leds);
+   shiftOut(dataPinG, clockPin, MSBFIRST, 253);
    digitalWrite(latchPin, HIGH);
 }
